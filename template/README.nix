386b623e96380de7e561fa19ab2cbc8063a0c32e
Samuel Tam <heiyiutam@gmail.com>
2024-04-22 01:48:48 +0800

    Update README.nix

--- template/README.nix
+++ template/README.nix
@@ -5,8 +5,8 @@
 
 writeTextDir "README.md" ''
   # nix-index-db
-  ### ${system} / ${channel} @ ${version}
-  - Nixpkgs: `${channel}` @ [`${shortRev}`](https://github.com/NixOS/nixpkgs/commit/${rev})
+  ### ${system}/${channel} @ ${version}
+  - Nixpkgs: `${channel}`@[`${shortRev}`](https://github.com/NixOS/nixpkgs/commit/${rev})
   - Platform: `${system}`
-  - Date: `${builtins.substring 0 4 lastModifiedDate}-${builtins.substring 4 2 lastModifiedDate}-${builtins.substring 6 2 lastModifiedDate}`
+  - Date: `${lastModifiedDate}`
 ''
