05f3887d51185066a21bc261a2f8800ce3e077ba
usertam <heiyiutam@gmail.com>
2024-05-03 00:09:06 +0800

    flake.nix: mainline

--- flake.nix
+++ flake.nix
@@ -5,24 +5,24 @@
   inputs."nixos-22.11".url = "github:nixos/nixpkgs/nixos-22.11";
 
   outputs = { self, ... }@inputs: let
-    forAllSystems = with inputs.nixpkgs-unstable.lib; genAttrs platforms.unix;
-    forAllChannels = drv: builtins.listToAttrs (map (ch: { name = ch; value = drv ch; }) (builtins.attrNames inputs));
+    inherit (inputs.nixpkgs-unstable.lib) genAttrs platforms;
+    forAllSystems = genAttrs platforms.unix;
+    forAllChannels = genAttrs (builtins.attrNames inputs);
   in {
     packages = forAllSystems (system-host:
       forAllSystems (system: forAllChannels (channel: let
         pkgs = inputs.nixpkgs-unstable.legacyPackages.${system-host};
         lock = with builtins; fromJSON (readFile ./flake.lock);
-      in pkgs.stdenv.mkDerivation rec {
+        date = inputs.${channel}.lastModifiedDate;
+        commit = inputs.${channel}.rev;
+        abbrev = inputs.${channel}.shortRev;
+      in pkgs.stdenv.mkDerivation (finalAttrs: {
         pname = "nix-index-db-src-${system}-${channel}";
-        version = builtins.substring 2 6 inputs.${channel}.lastModifiedDate
-          + "." + inputs.${channel}.shortRev;
+        version = builtins.substring 2 6 date + "." + abbrev;
         src = self;
-        # __noChroot = true;
-        # requiredSystemFeatures = [ "recursive-nix" ];
         nativeBuildInputs = [
           pkgs.nix-index
           pkgs.cacert
-          passthru.README
         ];
         dontUnpack = true;
         dontPatch = true;
@@ -30,18 +30,38 @@
         buildPhase = ''
           mkdir -p $out/nix-index-db
           HOME=$TMP nix-index --db $out/nix-index-db --system ${system} \
-            --nixpkgs https://github.com/NixOS/nixpkgs/tarball/${inputs.${channel}.rev}
+            --nixpkgs https://github.com/NixOS/nixpkgs/tarball/${commit}
         '';
         installPhase = ''
-          install -Dm444 -t $out $src/template/flake.nix $src/template/flake.lock \
-            ${passthru.README}/README.md
+          cat <<EOF > $out/flake.nix
+          {
+            outputs = { self }: let
+              system = with builtins; head (match "^### (.*)/.*" (readFile ./README.md));
+            in {
+              packages.''${system}.default = self;
+            };
+          }
+          EOF
+
+          cat <<EOF > $out/flake.lock
+          {
+            "nodes": {
+              "root": {}
+            },
+            "root": "root",
+            "version": 7
+          }
+          EOF
+
+          cat <<EOF > $out/README.md
+          # nix-index-db
+          ### ${system}/${channel} @ ${finalAttrs.version}
+          - Nixpkgs: `${channel}`@[`${abbrev}`](https://github.com/NixOS/nixpkgs/commit/${commit})
+          - Platform: `${system}`
+          - Date: `${date}`
+          EOF
         '';
-        passthru.README = import ./template/README.nix {
-          inherit (pkgs) writeTextDir;
-          inherit (inputs.${channel}) rev shortRev lastModifiedDate;
-          inherit system channel version;
-        };
-      })
+      }))
     ) // forAllChannels (channel:
       self.packages.${system-host}.${system-host}.${channel}
     ));
--- template/flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  outputs = { self }: let
-    system = with builtins; elemAt (match
-      "^.*Platform: `([a-z0-9_-]+)`.*$"
-      (readFile ./README.md)) 0;
-  in {
-    packages.${system}.default = ./.;
-  };
-}
